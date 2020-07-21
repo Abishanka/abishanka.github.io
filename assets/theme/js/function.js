0c2f724975d3f4ddb659319290a52bdbeef79cf6
Abishanka <57771060+Abishanka@users.noreply.github.com>
2020-07-21 22:15:02 +0530

    Theme change

---
+++ assets/theme/js/function.js
@@ -0,0 +1,115 @@
+function facebook() {
+	window.location.href = "https://facebook.com/abishanka.saha";
+}
+
+function twitter() {
+	window.location.href = "https://twitter.com/abishanka1";
+}
+function linkedin() {
+	window.location.href = "https://linkedin.com/in/abishanka";
+}
+function github() {
+	window.location.href = "https://github.com/abishanka";
+}
+
+function Index() {
+  var x = document.getElementById("index");
+      x.style.display = "block";
+  var x = document.getElementById("About");
+  x.style.display = "none";
+  var x = document.getElementById("Background");
+  x.style.display = "none";
+}
+
+function About() {
+	var x = document.getElementById("About");
+  x.style.display = "block";
+  var x = document.getElementById("index");
+  x.style.display = "none";
+  var x = document.getElementById("Background");
+  x.style.display = "none";
+}
+
+function Background() {
+  var x = document.getElementById("Background");
+  x.style.display = "block";
+  var x = document.getElementById("Research");
+    x.style.display = "none";
+  var x = document.getElementById("Award");
+    x.style.display = "none";
+  var x = document.getElementById("Experience");
+    x.style.display = "none";
+  var x = document.getElementById("Club");
+  x.style.display = "none";
+  var x = document.getElementById("index");
+  x.style.display = "none";
+  var x = document.getElementById("About");
+  x.style.display = "none";
+}
+
+function Education() {
+	var x = document.getElementById("Education");
+	x.style.display = "block";
+	var x = document.getElementById("Research");
+		x.style.display = "none";
+	var x = document.getElementById("Award");
+		x.style.display = "none";
+	var x = document.getElementById("Experience");
+		x.style.display = "none";
+	var x = document.getElementById("Club");
+	x.style.display = "none";
+ }
+
+function Research() {
+	var x = document.getElementById("Research");
+	x.style.display = "block";
+	var x = document.getElementById("Education");
+  	x.style.display = "none";
+ var x = document.getElementById("Award");
+  	x.style.display = "none";
+  var x = document.getElementById("Experience");
+  	x.style.display = "none";
+  var x = document.getElementById("Club");
+  	x.style.display = "none";
+ }
+
+ function Award() {
+ 	var x = document.getElementById("Award");
+	x.style.display = "block";
+  var x = document.getElementById("Education");
+  x.style.display = "none";
+ var x = document.getElementById("Research");
+  x.style.display = "none";
+  var x = document.getElementById("Experience");
+  x.style.display = "none";
+  var x = document.getElementById("Club");
+  x.style.display = "none";
+ }
+
+  function Experience() {
+  	var x = document.getElementById("Experience");
+    x.style.display = "block";
+    var x = document.getElementById("Education");
+    x.style.display = "none";
+    var x = document.getElementById("Research");
+    x.style.display = "none";
+    var x = document.getElementById("Award");
+    x.style.display = "none";
+    var x = document.getElementById("Club");
+    x.style.display = "none";
+ }
+
+  function Club() {
+  	var x = document.getElementById("Club");
+    x.style.display = "block";
+    var x = document.getElementById("Education");
+    x.style.display = "none";
+   var x = document.getElementById("Research");
+    x.style.display = "none";
+    var x = document.getElementById("Award");
+    x.style.display = "none";
+    var x = document.getElementById("Experience");
+    x.style.display = "none";
+ }
+
+
